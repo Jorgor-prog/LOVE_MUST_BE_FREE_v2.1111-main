55a65aea82cc4dd3fa0640a86bde1b24f7252946
Jorgor-prog <alpha9274@outlook.com>
2025-08-17 17:47:49 +0300

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,19 +2,18 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const ADMIN_LOGIN_ID = process.env.ADMIN_LOGIN_ID || 'Admin303';
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'T7#jZx9!rB2mLq4@';
+const ADMIN_LOGIN_PASSWORD = process.env.ADMIN_PASSWORD || 'T7#jZx9!rB2mLq4@';
 
 async function main() {
-
   await prisma.user.upsert({
     where: { loginId: ADMIN_LOGIN_ID },
     update: {
-      password: ADMIN_PASSWORD,
+      loginPassword: ADMIN_LOGIN_PASSWORD,
       role: 'ADMIN',
     },
     create: {
       loginId: ADMIN_LOGIN_ID,
-      password: ADMIN_PASSWORD,
+      loginPassword: ADMIN_LOGIN_PASSWORD,
       role: 'ADMIN',
     },
   });
