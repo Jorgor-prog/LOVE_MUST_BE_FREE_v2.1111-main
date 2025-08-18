263a9a4a67a92e91142f05629ea00c142c3829c4
Jorgor-prog <alpha9274@outlook.com>
2025-08-18 09:30:58 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import './globals.css';
-export const metadata = { title: 'LOVE MUST BE FREE', description: 'App' };
-export default function RootLayout({ children }:{ children: React.ReactNode }){
-  return (<html lang="en"><body>{children}</body></html>);
+export const metadata = { title: 'LOVE MUST BE FREE' };
+export default function RootLayout({children}:{children:React.ReactNode}){
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
