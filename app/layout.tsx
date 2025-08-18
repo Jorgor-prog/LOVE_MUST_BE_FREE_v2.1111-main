b27273d74a5c711fac14e0a32548b9e3dcf0b654
Jorgor-prog <alpha9274@outlook.com>
2025-08-18 10:09:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Love Must Be Free',
   description: 'Secure area',
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="lmf-bg">{children}</body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
